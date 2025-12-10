0bc3a8c06d9da2a30420b5a266b7852f3eab06b9
Benhanan14406 <a.b.abinaya.fuun@gmail.com>
2025-12-10 21:12:33 +0700

    profile page completed

--- lib/abi/models/CustomUser.dart
+++ lib/abi/models/CustomUser.dart
@@ -1,54 +1,44 @@
 import 'dart:convert';
 
-CustomUser customUserFromJson(String str) => CustomUser.fromJson(json.decode(str));
+// Use this function if your API returns a LIST of users
+List<CustomUser> customUserListFromJson(String str) =>
+    List<CustomUser>.from(json.decode(str).map((x) => CustomUser.fromJson(x)));
+
+// Use this function if your API returns a SINGLE user object
+CustomUser customUserFromJson(String str) =>
+    CustomUser.fromJson(json.decode(str));
 
 String customUserToJson(CustomUser data) => json.encode(data.toJson());
 
 class CustomUser {
-  List<CustomUserElement> customUser;
+  final String uuid;
+  final int userId; // Matches the 'user' OneToOneField (User ID)
+  final DateTime joinDate;
+  final String username;
+  final String? name;    // Nullable in Django
+  final String? picture; // Nullable in Django
 
   CustomUser({
-    required this.customUser,
+    required this.uuid,
+    required this.userId,
+    required this.joinDate,
+    required this.username,
+    this.name,
+    this.picture,
   });
 
   factory CustomUser.fromJson(Map<String, dynamic> json) => CustomUser(
-    customUser: List<CustomUserElement>.from(json["customUser"].map((x) => CustomUserElement.fromJson(x))),
-  );
-
-  Map<String, dynamic> toJson() => {
-    "customUser": List<dynamic>.from(customUser.map((x) => x.toJson())),
-  };
-}
-
-class CustomUserElement {
-  String uuid;
-  int user;
-  DateTime joinDate;
-  String username;
-  String name;
-  dynamic picture;
-
-  CustomUserElement({
-    required this.uuid,
-    required this.user,
-    required this.joinDate,
-    required this.username,
-    required this.name,
-    required this.picture,
-  });
-
-  factory CustomUserElement.fromJson(Map<String, dynamic> json) => CustomUserElement(
     uuid: json["uuid"],
-    user: json["user"],
+    userId: json["user"], // Django PK usually comes as "user"
     joinDate: DateTime.parse(json["join_date"]),
     username: json["username"],
-    name: json["name"],
-    picture: json["picture"],
+    name: json["name"], // Can be null
+    picture: json["picture"], // Can be null or URL string
   );
 
   Map<String, dynamic> toJson() => {
     "uuid": uuid,
-    "user": user,
+    "user": userId,
     "join_date": joinDate.toIso8601String(),
     "username": username,
     "name": name,
